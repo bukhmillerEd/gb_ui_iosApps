4dc82793df480b4fd7c86d49312bb1887231112e
Eduard Bukhmiller <bukhmillere@gmail.com>
2022-03-14 17:53:26 +0300

    Выполнил ДЗ 3

--- UIiosApps/UIiosApps/Models/User.swift
+++ UIiosApps/UIiosApps/Models/User.swift
@@ -8,8 +8,49 @@
 import Foundation
 import UIKit
 
-struct User {
-	let name: String
-	let avatar: UIImage?
-	let fotos: [UIImage?]
+class UserContainer: Decodable {
+	var users: [User]
+	
+	enum CodingKeys: String, CodingKey {
+		case response
+		
+		enum UsersKeys: String, CodingKey {
+			case users = "items"
+		}
+	}
+	
+	required init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		let response = try container.nestedContainer(keyedBy: CodingKeys.UsersKeys.self, forKey: .response)
+		self.users = try response.decode([User].self, forKey: .users)
+	}
 }
+
+class User: Decodable {
+	var id: Int
+	var name: String
+	var avatar: UIImage? = nil
+	var fotos: [UIImage?] = []
+	
+	enum CodingKeys: String, CodingKey {
+		case firstName = "first_name"
+		case lastName = "last_name"
+		case avatar = "photo_100"
+		case id
+	}
+	
+	required init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		
+		self.id = try container.decode(Int.self, forKey: .id)
+		
+		let firstName = try container.decode(String.self, forKey: .firstName)
+		let lastName = try container.decode(String.self, forKey: .lastName)
+		self.name = firstName + " " + lastName
+		
+		let urlAvatar = try container.decode(URL.self, forKey: .avatar)
+		self.avatar = UIImage(data: try Data(contentsOf: urlAvatar))
+	}
+	
+}
+
